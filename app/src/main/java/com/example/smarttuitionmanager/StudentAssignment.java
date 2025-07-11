1a04403a32380d53d222248c2c0e90aba3d36e63
Senesh-j <seneshjayamaha4@gmail.com>
2025-07-11 22:17:07 +0530

    the complete nav for the studet,  teacher

---
+++ app/src/main/java/com/example/smarttuitionmanager/StudentAssignment.java
@@ -0,0 +1,75 @@
+package com.example.smarttuitionmanager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class StudentAssignment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public StudentAssignment() {
+        // Required empty public constructor
+    }
+
+    public static StudentAssignment newInstance(String param1, String param2) {
+        StudentAssignment fragment = new StudentAssignment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_student_assignment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button courseBtn = view.findViewById(R.id.Cmaterial);
+        Button resultsBtn = view.findViewById(R.id.results);
+
+        courseBtn.setOnClickListener(v -> {
+            FragmentTransaction transaction = requireActivity()
+                    .getSupportFragmentManager()
+                    .beginTransaction();
+            transaction.replace(R.id.fragment_container, new StudentCourseGuide());
+            transaction.addToBackStack(null);
+            transaction.commit();
+        });
+
+        resultsBtn.setOnClickListener(v -> {
+            FragmentTransaction transaction = requireActivity()
+                    .getSupportFragmentManager()
+                    .beginTransaction();
+            transaction.replace(R.id.fragment_container, new StudentResults());
+            transaction.addToBackStack(null);
+            transaction.commit();
+        });
+    }
+}
